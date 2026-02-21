97b9e51a10414ecb76e2330cdd49e63d01796f42
Hirises <hirises1243@gmail.com>
2026-02-21 13:42:41 +0900

    feat: 범용 싱글톤 기능

--- Runtime/Singleton/PersistentSingletonBehaviour.cs
+++ Runtime/Singleton/PersistentSingletonBehaviour.cs
@@ -1,15 +1,17 @@
+using UnityEngine;
+
 namespace HUtil.Runtime.Singleton
 {
     /// <summary>
-    /// 런타임에 동적으로 생성되는 싱글톤 클래스
+    /// 런타임에 동적으로 생성되는 싱글톤 DontDestroyOnLoad 오브젝트
     /// </summary>
     /// <typeparam name="T">싱글톤 클래스</typeparam>
-    public class SingletonStaticClass<T> where T : SingletonStaticClass<T>, new()
+    public class PersistentSingletonBehaviour<T> : MonoBehaviour where T : PersistentSingletonBehaviour<T>
     {
         private static T _instance;
         /// <summary>
         /// 싱글톤 인스턴스<br>
-        /// 최초 get 시점에 최초 생성됩니다
+        /// 최초 get 시점에 씬에 자동으로 추가됩니다
         /// </summary>
         public static T Instance
         {
@@ -17,7 +19,9 @@ namespace HUtil.Runtime.Singleton
             {
                 if (_instance == null)
                 {
-                    _instance = new T();
+                    GameObject obj = new GameObject(typeof(T).Name);
+                    _instance = obj.AddComponent<T>();
+                    DontDestroyOnLoad(obj);
                 }
                 return _instance;
             }
--- Runtime/Singleton/SingletonBehaviour.cs
+++ Runtime/Singleton/SingletonBehaviour.cs
@@ -2,19 +2,35 @@ using UnityEngine;
 
 namespace HUtil.Runtime.Singleton
 {
-    public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    /// <summary>
+    /// 런타임에 바인딩되는 싱글톤 오브젝트
+    /// </summary>
+    /// <typeparam name="T">싱글톤 클래스</typeparam>
+    public class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T>
     {
         private static T _instance;
-        public static T Instance
+        /// <summary>
+        /// 싱글톤 인스턴스
+        /// </summary>
+        public static T Instance => _instance;
+
+        protected virtual void Awake()
         {
-            get
+            if (_instance == null)
             {
-                if (_instance == null)
-                {
-                    GameObject obj = new GameObject(typeof(T).Name);
-                    _instance = obj.AddComponent<T>();
-                }
-                return _instance;
+                _instance = this as T;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (_instance == this)
+            {
+                _instance = null;
             }
         }
     }
