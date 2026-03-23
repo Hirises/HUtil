abae53d45e069782232f09427e5dc32ddc4c9cf3
Hirises <hirises1243@gmail.com>
2026-03-23 23:07:57 +0900

    feat: multi activate binder 개선

--- UI/Binder/MultiActivationBinder.cs
+++ UI/Binder/MultiActivationBinder.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
 
+using HUtil.Runtime;
+using HUtil.Runtime.Extension;
 using HUtil.Runtime.Observable;
+
+using Sirenix.OdinInspector;
 
 using UnityEngine;
 
@@ -14,6 +18,8 @@ namespace HUtil.UI.Binder
     {
         [SerializeField] private ActivationState[] _activationStates;
         [SerializeField] private PropertyBindingPort<int> _index_prop = new PropertyBindingPort<int>(BindingType.OfType(BindingBaseType.Int), BindingDirectionFlags.ToUI);
+        [InfoBox("evaluateMultipleStates: true라면 모든 상태를 1회씩 체크합니다 (서로 중복되는 요소가 있을 경우 예기치 못한 동작이 발생할 수 있습니다)")]
+        [SerializeField] private bool evaluateMultipleStates = true;
 
         protected void OnValidate()
         {
@@ -29,10 +35,13 @@ namespace HUtil.UI.Binder
         {
             foreach (var state in _activationStates)
             {
+                bool isMatch = state._comparisonOperator.Compare(state._index, index);
+                if(!evaluateMultipleStates && !isMatch){
+                    continue;
+                }
                 foreach (var obj in state._onObjects)
                 {
-                    Debug.Log($"SetActive {obj.name} {state._index == index}");
-                    obj.SetActive(state._index == index);
+                    obj.SetActive(isMatch);
                 }
             }
         }
@@ -40,6 +49,7 @@ namespace HUtil.UI.Binder
         [Serializable]
         private class ActivationState{
             [SerializeField] public int _index;
+            [SerializeField] public ComparisonOperator _comparisonOperator;
             [SerializeField] public GameObject[] _onObjects;
         }
     }
